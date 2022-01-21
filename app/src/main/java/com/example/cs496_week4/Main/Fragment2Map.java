9118426f48c5f49c788648d4eae29e3e1d3b4ae4
Kim-Deokhyeon1 <93712189+Kim-Deokhyeon1@users.noreply.github.com>
2022-01-21 19:47:58 +0900

    Added Map

--- app/src/main/java/com/example/cs496_week4/Main/Fragment2Map.java
+++ app/src/main/java/com/example/cs496_week4/Main/Fragment2Map.java
@@ -1,16 +1,34 @@
 package com.example.cs496_week4.Main;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.example.cs496_week4.R;
+import com.naver.maps.map.LocationTrackingMode;
+import com.naver.maps.map.MapView;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.UiSettings;
+import com.naver.maps.map.util.FusedLocationSource;
 
-public class Fragment2Map extends Fragment {
+public class Fragment2Map extends Fragment implements OnMapReadyCallback {
     // fields
+    private static final int PERMISSION_REQUEST_CODE = 100;
+    private static final String[] PERMISSIONS = {
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+    };
+
+    private MapView mapView;
+    private FusedLocationSource mLocationSource;
+    private NaverMap mNaverMap;
 
     // Required empty public constructor
     public Fragment2Map() {
@@ -36,6 +54,54 @@ public class Fragment2Map extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_main_2, container, false);
 
+        mapView = view.findViewById(R.id.map_view);
+
+        naverMapBasicSettings();
+
+        mLocationSource =
+                new FusedLocationSource(this, PERMISSION_REQUEST_CODE);
+
         return view;
+    }
+
+    public void naverMapBasicSettings() {
+        mapView.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull final NaverMap naverMap) {
+
+
+
+        // 현재 위치 버튼 안보이게 설정
+        UiSettings uiSettings = naverMap.getUiSettings();
+
+        uiSettings.setLocationButtonEnabled(false);
+
+
+
+        // 지도 유형 설정
+        naverMap.setMapType(NaverMap.MapType.Basic);
+
+        // NaverMap 객체 받아서 NaverMap 객체에 위치 소스 지정
+        mNaverMap = naverMap;
+        mNaverMap.setLocationSource(mLocationSource);
+
+        // 권한확인. 결과는 onRequestPermissionsResult 콜백 매서드 호출
+        requestPermissions(PERMISSIONS, PERMISSION_REQUEST_CODE);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        // request code와 권한획득 여부 확인
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                mNaverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
+            }
+        }
     }
 }
